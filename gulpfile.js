1821241e215fa13e44146cac2a5fb052a9567f58
Harris Jose <harrisjose@outlook.com>
2017-04-23 23:17:07 +0530

    Add config for local dev

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ var gulp = require('gulp')
 var exec = require('child_process').exec
 var minify = require('gulp-minify')
 var minifyCSS = require('gulp-minify-css')
+var watch = require('gulp-watch')
+var server = require('gulp-server-livereload')
 
 gulp.task('buildHTML', function () {
   gulp.src('content/*.html')
@@ -15,7 +17,17 @@ gulp.task('buildCSS', function () {
   .pipe(gulp.dest('dist/css'))
 })
 
-gulp.task('build', ['buildHTML', 'buildCSS'], function () {
+gulp.task('buildJS', function () {
+  gulp.src('assets/css/*.js')
+  .pipe(gulp.dest('dist/js'))
+})
+
+gulp.task('copyStatic', function () {
+  gulp.src('assets/images/*.*')
+  .pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('build', ['buildHTML', 'buildCSS', 'copyStatic'], function () {
   console.log('Building Site')
 })
 
@@ -25,5 +37,40 @@ gulp.task('deploy', ['build'], function () {
     if (err) {
       console.error(err)
     }
+  })
+})
+
+gulp.task('devBuild', function () {
+  gulp.src('assets/css/*.css')
+  .pipe(gulp.dest('build/css'))
+
+  gulp.src('assets/images/*.*')
+  .pipe(gulp.dest('build/images'))
+
+  gulp.src('content/*.html')
+  .pipe(gulp.dest('build/'))
+})
+
+gulp.task('server', ['devBuild'], function () {
+  gulp.src('build')
+  .pipe(server({
+    livereload: true,
+    directoryListing: false,
+    open: true
+  }))
+  /* Watch files and run tasks to update files */
+  watch('assets/css/*.css', function (events, done) {
+    console.log('Building CSS files..')
+    gulp.start('buildCSS', done)
+  })
+
+  watch('assets/css/*.js', function (events, done) {
+    console.log('Building JS files..')
+    gulp.start('buildJS', done)
+  })
+
+  watch('content/*.html', function (events, done) {
+    console.log('Building HTML files..')
+    gulp.start('buildHTML', done)
   })
 })
